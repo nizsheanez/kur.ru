83a1f3eb541e36f5047e1c0c98e017571889716f
Alexey Sharov <alexey.sharov@mebelrama.ru>
2012-07-18 18:52:20 +0400

    sql up

--- sql.sql
+++ sql.sql
@@ -3,12 +3,18 @@
 -- http://www.phpmyadmin.net
 --
 -- Хост: 127.0.0.1
--- Время создания: Июл 18 2012 г., 18:48
+-- Время создания: Июл 18 2012 г., 18:51
 -- Версия сервера: 5.5.25
 -- Версия PHP: 5.3.13
 
 SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
 
 --
 -- База данных: `diplom`
@@ -217,3 +223,7 @@ CREATE TABLE IF NOT EXISTS `squares` (
 
 INSERT INTO `squares` (`id`, `title`) VALUES
 (1, 'Алматинский');
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
