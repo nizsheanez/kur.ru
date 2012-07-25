85f696100133922871c097320dcdbe07e2e74620
Alexey Sharov <alexey.sharov@mebelrama.ru>
2012-07-25 11:25:59 +0400

    #1 - optimize render of active data save form

--- protected/modules/regions/views/save/dataForm.php
+++ protected/modules/regions/views/save/dataForm.php
@@ -1,11 +1,17 @@
 <?php
-foreach ($sector->data as $item)
+$metric = Metric::model()->findByAttributes(array('name' => $metric));
+$metrics = $metric->descendants()->findAll();
+
+$ids = CHtml::listData($metrics, 'id', 'id');
+$ids[$metric->id] = $metric->id;
+$data = Data::model()->inCondition('metric_id', CHtml::listData($metrics, 'id', 'id'))->findAllByAttributes(array(
+    'sector_id' => $sector->id,
+));
+
+foreach ($data as $item)
 {
-    if ($item->metric->inSubtreeOf($metric) || $item->metric->type == null)
-    {
-        echo CHtml::label($item->metric->title, 'data['.$item->id.']');
-        echo '<br />';
-        echo CHtml::textField('data['.$item->id.']', $item->value);
-        echo '<br />';
-    }
+    echo CHtml::label($item->metric->title, 'data['.$item->id.']');
+    echo '<br />';
+    echo CHtml::textField('data['.$item->id.']', $item->value);
+    echo '<br />';
 }
