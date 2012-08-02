e2c3755f0730e73faf24b2b48a324b9bb9b3d9a0
Alexey Sharov <alexey.sharov@mebelrama.ru>
2012-08-02 13:30:56 +0400

    merge

--- protected/modules/regions/models/Metric.php
+++ protected/modules/regions/models/Metric.php
@@ -62,53 +62,6 @@ class Metric extends ActiveRecord
     }
 
 
-    public static function getHtmlTree()
-    {
-        $models = self::getRoot()->descendants()->findAll();
-
-        $depth = 0;
-        $res   = '';
-
-        foreach ($models as $n=> $item)
-        {
-            if ($item->depth == $depth)
-            {
-                $res .= CHtml::closeTag('li') . "\n";
-            }
-            else if ($item->depth > $depth)
-            {
-                $res .= CHtml::openTag('ul', array('class' => 'depth_' . $item->depth)) . "\n";
-            }
-            else
-            {
-                $res .= CHtml::closeTag('li') . "\n";
-
-                for ($i = $depth - $item->depth; $i; $i--)
-                {
-                    $res .= CHtml::closeTag('ul') . "\n";
-                    $res .= CHtml::closeTag('li') . "\n";
-                }
-            }
-
-            $res .= CHtml::openTag('li', array(
-                'id'   => 'items_' . $item->id,
-                'class'=> 'depth_' . $item->depth
-            ));
-            $res .= CHtml::tag('div', array(), CHtml::encode($item->title) .
-                '<img class="drag" src="/img/admin/hand.png" height="16" width="16" />');
-            $depth = $item->depth;
-        }
-
-        for ($i = $depth; $i; $i--)
-        {
-            $res .= CHtml::closeTag('li') . "\n";
-            $res .= CHtml::closeTag('ul') . "\n";
-        }
-
-        return $res;
-    }
-
-
     public static function getRoot()
     {
         return self::model()->roots()->find();
--- protected/modules/regions/portlets/NestedTree.php
+++ protected/modules/regions/portlets/NestedTree.php
@@ -69,8 +69,9 @@ class NestedTree extends Portlet
         {
             $settings = CJavaScript::encode($this->sortingSettings);
             $cs->registerScriptFile($plugins . 'nestedSortable/nestedSortable.js')->registerCssFile(
-                $plugins . 'nestedSortable/nestedSortable.css')->registerScriptFile(
-                $plugins . 'toJson/toJson.js')->registerScript(
+                $plugins . 'nestedSortable/nestedSortable.css')
+                ->registerScriptFile($plugins . 'toJson/toJson.js')
+                ->registerScript(
                 $this->id . 'NestedTreeSortable', "$('#{$this->id} > ul').nestedSortable({$settings})");
         }
     }
@@ -79,8 +80,53 @@ class NestedTree extends Portlet
     public function renderContent()
     {
         $this->render('NestedTree', array(
-            'tree' => $this->model->htmlTree
+            'tree' => $this->htmlTree
         ));
     }
 
+    public function getHtmlTree()
+    {
+        $models = $this->model->model()->getRoot()->descendants()->findAll();
+
+        $depth = 0;
+        $res   = '';
+
+        foreach ($models as $n=> $item)
+        {
+            if ($item->depth == $depth)
+            {
+                $res .= CHtml::closeTag('li') . "\n";
+            }
+            else if ($item->depth > $depth)
+            {
+                $res .= CHtml::openTag('ul', array('class' => 'depth_' . $item->depth)) . "\n";
+            }
+            else
+            {
+                $res .= CHtml::closeTag('li') . "\n";
+
+                for ($i = $depth - $item->depth; $i; $i--)
+                {
+                    $res .= CHtml::closeTag('ul') . "\n";
+                    $res .= CHtml::closeTag('li') . "\n";
+                }
+            }
+
+            $res .= CHtml::openTag('li', array(
+                'id'   => 'items_' . $item->id,
+                'class'=> 'depth_' . $item->depth
+            ));
+            $res .= CHtml::tag('div', array(), CHtml::encode($item->title) .
+                '<img class="drag" src="/img/admin/hand.png" height="16" width="16" />');
+            $depth = $item->depth;
+        }
+
+        for ($i = $depth; $i; $i--)
+        {
+            $res .= CHtml::closeTag('li') . "\n";
+            $res .= CHtml::closeTag('ul') . "\n";
+        }
+
+        return $res;
+    }
 }
